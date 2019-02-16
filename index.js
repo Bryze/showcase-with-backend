ff4872a660426a027d276a8a27c9aaecbeeefb00
Aseem Upadhyay <aseem2301@gmail.com>
2019-02-16 20:19:44 +0530

    plugged in react to be used by express server

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/api/', cors(), async (req, res, next) => {
-  try {
-    const message = { text: 'Hello' };
-    res.json({ message })
-  } catch (err) {
-    next(err)
-  }
-})
+    try {
+        const message = {text: 'Hello'};
+        res.json({message})
+    } catch (err) {
+        next(err)
+    }
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Mixing it up on port ${PORT}`)
-})
+    console.log(`Mixing it up on port ${PORT}`)
+});
