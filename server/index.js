391dcb64cd1a70b77305b90ddbc9561da91fb8d0
Aseem Upadhyay <aseem2301@gmail.com>
2019-02-17 14:56:58 +0530

    fixed incorrect build route for react app to be rendered by express

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 require('./routes')(app);
-
-app.use(express.static(path.join(__dirname, 'client/build')));
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, './../client/build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+    res.sendFile(path.join(__dirname + './../client/build/index.html'))
 });
 
 const PORT = process.env.PORT || 5000;
